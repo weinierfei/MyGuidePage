b8ecd04c5c2f7398705cd22da17973a77557f5d9
guoyongping <gweinierfei@126.com>
2015-08-26 22:27:42 +0800

    +测试2222

--- app/src/main/java/com/ty/myguidepage/TestGitActivity.java
+++ app/src/main/java/com/ty/myguidepage/TestGitActivity.java
@@ -1,9 +1,11 @@
 package com.ty.myguidepage;
 
+import android.app.Activity;
+
 /**
  * Description:
  * User: guoyongping
  * Date: 2015-08-26
  */
-public class TestGitActivity {
+public class TestGitActivity extends Activity{
 }
